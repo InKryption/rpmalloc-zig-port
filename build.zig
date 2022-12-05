976af129f7ce8196c56571da6064ca95cdaa1952
InKryption <inkryption07@gmail.com>
2022-12-05 23:44:38 +0100

    Improve build.zig

--- build.zig
+++ build.zig
@@ -5,33 +5,47 @@ pub fn build(b: *std.build.Builder) void {
     const target = b.standardTargetOptions(.{});
 
     const bench_implementation = b.option(enum { zig, c }, "impl", "Which impl of the benchmark to run") orelse .zig;
-    const strip = b.option(bool, "strip", "Strip executable") orelse (mode != .Debug);
+    const strip = b.option(bool, "strip", "Strip executable");
+    const want_lto = b.option(bool, "want-lto", "Enable wanting LTO");
 
-    const bench_impl_leo = b.addObject("benchmark-impl", "src/benchmark.zig");
-    bench_impl_leo.setBuildMode(mode);
-    bench_impl_leo.setTarget(target);
-    bench_impl_leo.strip = strip;
+    const zig_bench_impl_leo = b.addSharedLibrary("benchmark-impl-zig", "src/benchmark.zig", .unversioned);
+    zig_bench_impl_leo.setBuildMode(mode);
+    zig_bench_impl_leo.setTarget(target);
+    zig_bench_impl_leo.strip = strip;
+    zig_bench_impl_leo.want_lto = want_lto;
+
+    const c_bench_impl_leo = b.addSharedLibrary("benchmark-impl-c", null, .unversioned);
+    c_bench_impl_leo.setBuildMode(mode);
+    c_bench_impl_leo.setTarget(target);
+    c_bench_impl_leo.strip = strip;
+    c_bench_impl_leo.want_lto = want_lto;
+    c_bench_impl_leo.addIncludePath("rpmalloc-benchmark/benchmark");
+    c_bench_impl_leo.addIncludePath("rpmalloc-benchmark/test");
+    c_bench_impl_leo.addCSourceFiles(&.{
+        "rpmalloc-benchmark/benchmark/rpmalloc/benchmark.c",
+        "rpmalloc-benchmark/benchmark/rpmalloc/rpmalloc.c",
+    }, &.{"-O3"});
+    c_bench_impl_leo.linkLibC();
 
     const bench_leo = b.addExecutable(switch (bench_implementation) {
         inline else => |tag| "benchmark-" ++ @tagName(tag),
     }, "rpmalloc-benchmark/benchmark/main.c");
     bench_leo.setBuildMode(mode);
     bench_leo.setTarget(target);
+    bench_leo.strip = strip;
+    bench_leo.want_lto = want_lto;
 
+    bench_leo.linkLibC();
     bench_leo.addIncludePath("rpmalloc-benchmark/benchmark");
     bench_leo.addIncludePath("rpmalloc-benchmark/test");
-    bench_leo.linkLibC();
     bench_leo.addCSourceFiles(&.{
         "rpmalloc-benchmark/test/thread.c",
         "rpmalloc-benchmark/test/timer.c",
-    }, &.{ "-O3" });
+    }, &.{"-O3"});
 
     switch (bench_implementation) {
-        .zig => bench_leo.addObject(bench_impl_leo),
-        .c => bench_leo.addCSourceFiles(&.{
-            "rpmalloc-benchmark/benchmark/rpmalloc/benchmark.c",
-            "rpmalloc-benchmark/benchmark/rpmalloc/rpmalloc.c",
-        }, &.{ "-O3" }),
+        .zig => bench_leo.linkLibrary(zig_bench_impl_leo),
+        .c => bench_leo.linkLibrary(c_bench_impl_leo),
     }
     bench_leo.install();
 
